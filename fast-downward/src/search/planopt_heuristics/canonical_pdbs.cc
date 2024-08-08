01ff9254f2d6f4a5acbb2a4602f09d4b1fcd996e
Izaque Dione Nunes <68032837+IzaqueNunes@users.noreply.github.com>
2024-08-08 10:45:42 -0300

    Update canonical_pdbs.cc

--- fast-downward/src/search/planopt_heuristics/canonical_pdbs.cc
+++ fast-downward/src/search/planopt_heuristics/canonical_pdbs.cc
@@ -91,13 +91,10 @@ int CanonicalPatternDatabases::compute_heuristic(const TNFState &original_state)
 
        // TODO: add your code for exercise (d) here.
        
-       for (const auto &clique : maximal_additive_sets) {
+       for (const PatternClique &clique : maximal_additive_sets) {
 	        int clique_sum = 0;
 	        for (int pdb_index : clique) {
 	            clique_sum += heuristic_values[pdb_index];
-	            if (clique_sum < 0) { // Verificação de overflow
-	                return numeric_limits<int>::max();
-	            }
 	        }
 	        h = max(h, clique_sum);
     	}
